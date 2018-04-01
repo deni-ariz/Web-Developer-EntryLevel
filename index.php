b69d2d2495371a8b7d46db1e93ece25e97eaf8da
Kenneth Dennis <37792873+deni-ariz@users.noreply.github.com>
2018-03-31 18:23:03 -0700

    Update and rename Felix - FavCode Test.html to index.php

---
+++ index.php
@@ -0,0 +1,149 @@
+<!DOCTYPE html>
+<!-- saved from url=(0027)http://apply.felixihub.com/ -->
+<html lang="en">
+	<head>
+		<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+
+	
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+	<title>Felix - FavCode Test</title>
+
+	<meta name="description" content="African development through partnership">
+
+
+	<!-- Open Graph data -->
+	<meta property="og:title" content="Job Application Submission for Felix - Favcode ">
+	<meta property="og:type" content="article">
+	<meta property="og:description" content="African development through partnership">
+
+	<!--Fonts-->
+	<link href="https://fonts.googleapis.com/css?family=Lobster" rel="stylesheet" type="text/css">
+	
+
+	<!-- css styling-->
+	<style>
+		.top_bar {
+    background: #000000;
+    color: white;
+    font-weight: bold;
+    line-height: 20px;
+	padding: 10px;
+    border-radius: 10px 10px 0px 0px;
+}
+
+.normal_bar{
+	background: #0033cc;
+    color: white;
+    line-height: 20px;
+	padding: 10px;
+}
+.normal_bar a{
+    color: white;
+}
+.normal_bar a:visited{
+    color: white;
+}
+
+	.smaller-image {
+			width: 350px;
+			border-radius: 50%;
+}
+
+ h2.sansserif {
+ font-family: "times new roman georgia", "arial verdana", sans-serif;
+font-size: 60px;
+}
+
+.footer_bar{
+	margin-top: 5px;
+	padding:  10px 0px;
+	border-radius: 0px 0px 10px 10px;
+	font-size: 14px;
+	font-weight: bold;
+	text-align: center;
+	color: white;
+	background-color: #000000;
+}
+
+	</style>
+	<!--css styling ends-->
+</head>
+	
+<body>
+		<!-- header tag containing image and head text-->
+		<header>
+		<!--image-->
+			<div align="center">
+				<img class="smaller-image thicker-black-border"src="https://scontent.flos5-1.fna.fbcdn.net/v/t1.0-9/29597639_2275372499355976_8640071935848188603_n.jpg?_nc_cat=0&oh=0a49cd58b07438f080b0f8a648106213&oe=5B28065F"/>
+			</div>
+			<!--image ends-->
+			
+			<!--head text with background design-->
+			<div class="top_bar">
+			<h1 align="center"><span>
+				I am </span><span style="color: blue"><strong>Kenneth Dennis</strong></span>
+				</div>
+			</h1>
+		</header>
+		<!--end of header-->
+	
+		<section class="normal_bar">
+			<div>
+			<!--beginning of about me section-->
+                <h2 class="sansserif" align="center">
+			<strong>About Me</strong><hr/>
+				</h2>
+				<p>
+					<strong><font size="+2">Full Name:</font></strong>
+				<span>
+					<font size="+1">Kenneth Dennis Ifeanyichukwu</font>
+					</span>
+				</p>
+				<p>
+					<strong><font size="+2">Education:</font></strong>
+				<ul>
+				<font size="+1">
+				<li>FavCode 2018 V2 Applicant</li>
+				<li>Favcode A.I Student</li>
+				<li>St. Bridget's College(2010 - 2012)</li>
+				<li>Living Word Academy Secondary School Abayi-Ugwuala(2012 - 2017)</li>
+				</font>
+				</ul>
+				</p>
+				<p>
+					<strong>
+						<font size="+2">Mission/Objective for Favcode:</font>
+					</strong><br/>
+				<span>
+				<font size="+1">I am  a favcode 2018 V2 applicant and I want to use the knowledge that I will gain from Favcode to build beautiful and elegant web and software products.<br/>
+				I want to spearhead the products of the next innovative production and also bring technological advancement to Africa.</font>
+				</span>
+				</p>
+				
+                <h3>
+			<strong>
+				<font size="+2">This Is Why You Should Choose Me</font>
+			</strong>
+				</h3>
+                <p>
+			<font size="+1">I am a hardworking and down to earth person with deep passion for web and software development. I am always looking to grow as a person in my professional and personal life.<br/>
+				I want to be spearhead to creation of innovative products that will bring technological advancement to africa. I always want to learn and keep on refreshing my knowledge and my skill set
+				I am young, hungry and passionate about Tech thats why I am always looking for opportunities.</font>
+				</p>
+
+                     <!--social media section-->
+				<p>
+					<font size="+1">Here are some social media platforms which you can use to contact me</font>
+				</p>
+				<ul>
+				<li><a href="https://github.com/deni-ariz">Github</a></li>
+				<li><a href="https://web.facebook.com/arizdenken">facebook</a></li>
+				<li><a href="https://www.instagram.com/arizzden">Instagram</a></li></section>
+			</div>
+		<!-- Footer section-->
+		<footer class="footer_bar">
+			<p>March 31, 2018. Submission for Job With Felix - Favcode Partnership</p>
+			<p>Made by Kenneth Dennis.</p>
+		</footer>
+</body>
+</html>
